00eee717b1442919fc4895c17946c81bf3276afa
mikeraymond7 <michael.raymond1@comcast.net>
2022-05-16 16:14:22 -0400

    Implemented Static HTML/JS Functionality

--- ProjectRaymondMichael/wwwroot/js/site.js
+++ ProjectRaymondMichael/wwwroot/js/site.js
@@ -13,14 +13,63 @@
 }
 
 function displayInfo() {
+    dispLightsaber();
     var age = document.getElementById("age").value;
     var name = document.getElementById("name").value;
+
     var planet = document.getElementById("planet").value;
-    document.getElementById("disp").innerHTML = "You are "+age + " years old!<br>" + "Your name is "+ name + "? Strange name...<br> and you're from " + planet + "!<br>That's crazy!";
+    var fav = getJedi();
+    var padawan = "";
+
+    var ageStr = "You are " + age + " and ";
+    if (age == "") {
+        ageStr = ""
+    }
+    var nameStr = "Hello " + name;
+    if (name == "") {
+        if (planet != "") {
+            nameStr = "You are of planet " + planet;
+        }
+        else {
+            nameStr = "Hello";
+        }
+    }
+    else if (planet != ""){
+        nameStr += " of planet "+planet;
+    }
+   
+    if (document.getElementById("padawan").checked) {
+        padawan = "Welcome young padawan!<br>";
+    }
+    document.getElementById("disp").innerHTML = padawan + nameStr + "<br>" + ageStr + fav + " is your favorite Jedi!";
     $('#dispDiv').show();
 }
 
+function getJedi() {
+    var jedi = document.getElementsByName("jedi");
+    var fav = "someone";
+    for (var i = 0; i < jedi.length; i++) {
+        if (jedi[i].checked) {
+            fav = jedi[i].value;
+            break;
+        }
+    }
+    return fav;
+}
+
+function dispLightsaber() {
+    var color = document.getElementById("color").value;
+    $('.lightsaber').hide();
+    $('#' + color + "lightsaber").show();
+}
+
 function clearData() {
+    var jedi = document.getElementsByName("jedi");
+    for (var i = 0; i < jedi.length; i++) {
+        jedi[i].checked = false;
+    }
+
+    document.getElementById("padawan").checked = false;
     document.getElementById("age").value = "";
     document.getElementById("name").value = "";
     document.getElementById("planet").value = "";
@@ -29,4 +78,5 @@ function clearData() {
     $('#prelim').show();
     $('header').hide();
     $('footer').hide();
+    $('.lightsaber').hide();
 }
