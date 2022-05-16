49bf3e59e7d7147117df35be3b96f73458a1fcbc
mikeraymond7 <michael.raymond1@comcast.net>
2022-05-16 00:30:17 -0400

    Finished all Functionality

--- ProjectRaymondMichael/wwwroot/js/site.js
+++ ProjectRaymondMichael/wwwroot/js/site.js
@@ -7,14 +7,26 @@
         alert("Fantastic! Let us begin...");
         $('#prelim').hide();
         $('#main').show();
+        $('header').show();
+        $('footer').show();
     }
 }
 
-function getItems() {
-    var budget = document.getElementById("budget").value;
-    if (budget < 1) {
-        budget = 0;
-    }
-    alert(budget);
+function displayInfo() {
+    var age = document.getElementById("age").value;
+    var name = document.getElementById("name").value;
+    var planet = document.getElementById("planet").value;
+    document.getElementById("disp").innerHTML = "You are "+age + " years old!<br>" + "Your name is "+ name + "? Strange name...<br> and you're from " + planet + "!<br>That's crazy!";
+    $('#dispDiv').show();
 }
 
+function clearData() {
+    document.getElementById("age").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("planet").value = "";
+    $('#main').hide();
+    $('#dispDiv').hide();
+    $('#prelim').show();
+    $('header').hide();
+    $('footer').hide();
+}
